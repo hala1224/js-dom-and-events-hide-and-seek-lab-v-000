4bee7df6fed97bf883c5959d7b9ac796aa96488c
Natalie Halawani <hala1224@gmail.com>
2018-11-14 15:02:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,4 +16,10 @@ function increaseRankBy(n){
 
  function deepestChild() {
    const lis=document.getElementById("grand-node").querySelectorAll("div");
+   var test;
+   
+  for(let i=0; i<lis.length-1; i++) {
+    test=lis[i].querySelector("div");
+  }
+  return test;
  }
